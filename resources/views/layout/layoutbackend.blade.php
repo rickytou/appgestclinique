6d7748e879919fb53984f79b9704261879251ae1
Ricardo Samedi <samedi.ricardo@gmail.com>
2023-10-22 23:06:06 -0400

    Mises a jour 1 de l'interface UI du backend

--- resources/views/layout/layoutbackend.blade.php
+++ resources/views/layout/layoutbackend.blade.php
@@ -73,7 +73,7 @@
         </ul>       
         <!-- Fin Menu Login -->
         <!-- Logo Dashboard -->
-        <p class="dashboardIcon">
+        <p class="dashboardIcon active">
           <span class="fa-solid fa-gauge-high"></span>
           <a href="#">Dashboard</a>
         </p>
@@ -121,17 +121,222 @@
         <div class="resume-group">
           <!-- Bloc 1 -->
           <div class="resume-group-bloc">
-            <span></span>
+            <span class="fa-solid fa-stethoscope"></span>
             <b>Medecins</b>
             <span>3,973</span>
           </div>
           <!-- Fin bloc 1 -->
+
+           <!-- Bloc 2 -->
+           <div class="resume-group-bloc">
+            <span class="fa-solid fa-stethoscope"></span>
+            <b>Medecins</b>
+            <span>3,973</span>
+          </div>
+          <!-- Fin bloc 2 -->
+
+
+           <!-- Bloc 3 -->
+           <div class="resume-group-bloc">
+            <span class="fa-solid fa-stethoscope"></span>
+            <b>Medecins</b>
+            <span>3,973</span>
+          </div>
+          <!-- Fin bloc 3 -->
+
+
+           <!-- Bloc 4 -->
+           <div class="resume-group-bloc">
+            <span class="fa-solid fa-stethoscope"></span>
+            <b>Medecins</b>
+            <span>3,973</span>
+          </div>
+          <!-- Fin bloc 4 -->
         </div>
 
       </div>
 
+      <div class="mainlist">
+        <div class="mainlist-center">
+          <!-- Mainlist-center-left -->
+          <div class="mainlist-center-left">
+            <span class="mainlist-center-title">Medecins disponibles</span>  
+  
+            <!-- Medecin 1 -->
+            <div class="account-login listMedecin">
+              <!-- Photo login connected -->
+              <p class="account-login-profil">
+                <img src="{{url('assets/images/avatar.png')}}" alt="profil de l'utilisateur">
+              </p>
+              <!-- Fin Photo Login Connected -->
+              <!-- Nom user connected -->
+              <div class="account-login-username">
+                <strong>Dr. Cadet</strong>
+                <p>
+                  <span>Ophtalmologue</span>
+                </p>
+              </div>
+              <!-- Fin Nom user connected -->
+            </div>  
+            <!-- Fin Medecin 1 -->  
+            
+             <!-- Medecin 1 -->
+             <div class="account-login listMedecin">
+              <!-- Photo login connected -->
+              <p class="account-login-profil">
+                <img src="{{url('assets/images/avatar.png')}}" alt="profil de l'utilisateur">
+              </p>
+              <!-- Fin Photo Login Connected -->
+              <!-- Nom user connected -->
+              <div class="account-login-username">
+                <strong>Dr. Cadet</strong>
+                <p>
+                  <span>Ophtalmologue</span>
+                </p>
+              </div>
+              <!-- Fin Nom user connected -->
+            </div>  
+            <!-- Fin Medecin 1 -->  
+
+             <!-- Medecin 1 -->
+             <div class="account-login listMedecin">
+              <!-- Photo login connected -->
+              <p class="account-login-profil">
+                <img src="{{url('assets/images/avatar.png')}}" alt="profil de l'utilisateur">
+              </p>
+              <!-- Fin Photo Login Connected -->
+              <!-- Nom user connected -->
+              <div class="account-login-username">
+                <strong>Dr. Cadet</strong>
+                <p>
+                  <span>Ophtalmologue</span>
+                </p>
+              </div>
+              <!-- Fin Nom user connected -->
+            </div>  
+            <!-- Fin Medecin 1 -->  
+
+             <!-- Medecin 1 -->
+             <div class="account-login listMedecin">
+              <!-- Photo login connected -->
+              <p class="account-login-profil">
+                <img src="{{url('assets/images/avatar.png')}}" alt="profil de l'utilisateur">
+              </p>
+              <!-- Fin Photo Login Connected -->
+              <!-- Nom user connected -->
+              <div class="account-login-username">
+                <strong>Dr. Cadet</strong>
+                <p>
+                  <span>Ophtalmologue</span>
+                </p>
+              </div>
+              <!-- Fin Nom user connected -->
+            </div>  
+            <!-- Fin Medecin 1 -->  
+            
+          </div>
+          <!-- Fin MainList-center-left -->
+          <!-- Mainlist-center-left -->
+          <div class="mainlist-center-right">
+            <span class="mainlist-center-title">Nos r&eacute;cents rendez-vous</span>
+            <table>
+              <thead>
+                <tr>
+                  <th>Patient</th>
+                  <th>Medecin</th>
+                  <th>Date</th>
+                  <th>Heure</th>
+                  <th>T&eacute;l&eacute;phone</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Peter</td>
+                  <td>Dr Chang.</td>
+                  <td>22 dec 2023 </td>
+                  <td>10:30 AM</td>
+                  <td>438-9001245</td>
+                </tr>
+                <tr>
+                  <td>Peter</td>
+                  <td>Dr Chang.</td>
+                  <td>22 dec 2023 </td>
+                  <td>10:30 AM</td>
+                  <td>438-9001245</td>
+                </tr>
+                <tr>
+                  <td>Peter</td>
+                  <td>Dr Chang.</td>
+                  <td>22 dec 2023 </td>
+                  <td>10:30 AM</td>
+                  <td>438-9001245</td>
+                </tr> <tr>
+                  <td>Peter</td>
+                  <td>Dr Chang.</td>
+                  <td>22 dec 2023 </td>
+                  <td>10:30 AM</td>
+                  <td>438-9001245</td>
+                </tr> <tr>
+                  <td>Peter</td>
+                  <td>Dr Chang.</td>
+                  <td>22 dec 2023 </td>
+                  <td>10:30 AM</td>
+                  <td>438-9001245</td>
+                </tr>
+                <tr>
+                  <td>Peter</td>
+                  <td>Dr Chang.</td>
+                  <td>22 dec 2023 </td>
+                  <td>10:30 AM</td>
+                  <td>438-9001245</td>
+                </tr>
+                <tr>
+                  <td>Peter</td>
+                  <td>Dr Chang.</td>
+                  <td>22 dec 2023 </td>
+                  <td>10:30 AM</td>
+                  <td>438-9001245</td>
+                </tr>
+                <tr>
+                  <td>Peter</td>
+                  <td>Dr Chang.</td>
+                  <td>22 dec 2023 </td>
+                  <td>10:30 AM</td>
+                  <td>438-9001245</td>
+                </tr> <tr>
+                  <td>Peter</td>
+                  <td>Dr Chang.</td>
+                  <td>22 dec 2023 </td>
+                  <td>10:30 AM</td>
+                  <td>438-9001245</td>
+                </tr>
+                <tr>
+                  <td>Peter</td>
+                  <td>Dr Chang.</td>
+                  <td>22 dec 2023 </td>
+                  <td>10:30 AM</td>
+                  <td>438-9001245</td>
+                </tr>
+                <tr>
+                  <td>Peter</td>
+                  <td>Dr Chang.</td>
+                  <td>22 dec 2023 </td>
+                  <td>10:30 AM</td>
+                  <td>438-9001245</td>
+                </tr>
+              </tbody>            
+            </table>
+          </div>
+          <!-- Fin MainList-center-right -->
+        </div>
+      </div>
+      
     </main>
-    
+  <footer>
+    <div class="footer">
+      <p>Copyright &copy; 2023. Tous droits r&eacute;serv&eacutes | Ricardo Samedi </p>  
+    </div>  
+  </footer>  
     
   </div>
   <script
